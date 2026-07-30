94308f3eeee0eacaa1ad0a5601b9febb8f0d7aac
fuad-266 <fuadabdala266@gmail.com>
2026-07-30 21:41:33 +0300

    started the mobile application and working on that

--- retail-mobile/src/services/api.ts
+++ retail-mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import * as SecureStore from 'expo-secure-store'
 
-const API_BASE_URL = 'http://192.168.1.100:8080/api' // Replace with your backend IP
+const API_BASE_URL = 'http://localhost:8080/api' // Replace with your backend IP
 
 const api = axios.create({
   baseURL: API_BASE_URL,
